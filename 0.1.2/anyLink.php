e2f3b68ed60363ea508e405f4c7ed884128d5905
SivaDu <SivaDu@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-10-01 11:41:00 +0000

    Fixed some bugs in previous version Customed post type posts supported Javascript to redirect a page is available now
    git-svn-id: https://plugins.svn.wordpress.org/anylink/tags@780849 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ 0.1.2/anyLink.php
@@ -0,0 +1,27 @@
+<?php
+/*
+Plugin Name: anyLink
+Plugin URI: http://dudo.org/anylink
+Description: anyLink is an external links management tool. It help you to covert all the external links in your posts into internal links automatically. It can prevent the website weight flow outside to others. It's absolutely SEO friendly.
+Version: 0.1.2
+Author: dudo
+Author URI: http://dudo.org/about
+License: GPL2 or later
+*/
+defined( 'ABSPATH' ) OR exit;
+require_once( 'config.php' );
+require_once( ANYLNK_PATH . '/classes/al_covert.php' );
+require_once( ANYLNK_PATH . '/classes/al_filter.php' );
+require_once( ANYLNK_PATH . '/classes/al_slug.php' );
+require_once( 'functions.php' );
+require_once( ANYLNK_PATH . '/classes/al_option.php' );
+register_activation_hook( __FILE__, 'anylnkInstall' );
+$covert = new al_covert();
+$filter = new al_filter();
+$alOption = new al_option();
+add_action( 'publish_post', array( &$covert, 'covertURLs' ) );
+add_filter( 'the_content',  array( &$filter, 'applyFilter' ) );
+add_filter('query_vars', array( &$filter, 'addQueryVars' ) );
+add_action( 'parse_request', array( &$filter, 'alter_the_query' ) );
+add_action( 'plugins_loaded', 'al_load_textdomain' );
+?>
---
+++ 0.1.2/functions.php
@@ -0,0 +1,95 @@
+<?php
+//function to debug plugins
+defined( 'ABSPATH' ) OR exit;
+if(!function_exists('_log')){
+  function _log( $str = '', $message ) {
+    if( WP_DEBUG === true ){
+      if( is_array( $message ) || is_object( $message ) ){
+		error_log( $str );
+        error_log( print_r( $message, true ) );
+      } else {
+		error_log( $str );
+        error_log( $message );
+      }
+    }
+  }
+}
+function indexOf( $substr, $str ) {
+	if( strpos( $str, $substr ) === 0 )
+		return true;
+	else
+		return false;
+}
+function index2Of( $str1, $str2 ) {
+	if( indexOf( $str1, $str2 ) || indexOf( $str2, $str1 ) )
+		return true;
+	else
+		return false;
+}
+//Install
+function anylnkInstall() {
+	global $wpdb;
+	//create tables
+	require_once( ABSPATH . 'wp-admin/includes/upgrade.php' );
+	$charset_collate = '';
+	if( $wpdb->has_cap( 'collation' ) ){
+		if( ! empty( $wpdb->charset ) )
+		$charset_collate = "DEFAULT CHARACTER SET $wpdb->charset";
+		if( ! empty( $wpdb->collate ) )
+		$charset_collate .= " COLLATE $wpdb -> collate";
+	}
+	$sqlAnylnk = "CREATE TABLE " . ANYLNK_DBTB . " (
+		al_id mediumint(8) NOT NULL AUTO_INCREMENT,
+		al_slug varchar(255) NOT NULL,
+		al_crtime datetime NOT NULL,
+		al_origURL text NOT NULL,
+		al_count mediumint(8) DEFAULT 0 NOT NULL,
+		al_isAuto boolean DEFAULT 1,
+		PRIMARY KEY  (al_id),
+		KEY al_count (al_count),
+		KEY al_slug (al_slug)
+	) {$charset_collate};";
+	$sqlIndex = "CREATE TABLE " . ANYLNK_DBINDEX. " (
+		al_index_id mediumint(8) NOT NULL AUTO_INCREMENT,
+		al_url_id mediumint(8) NOT NULL,
+		al_post_id mediumint(8) NOT NULL,
+		al_comm_id mediumint(8) DEFAULT 0 NOT NULL,
+		PRIMARY KEY  (al_index_id),
+		KEY al_url_id (al_url_id),
+		KEY al_post_id (al_post_id),
+		KEY al_comm_id (al_comm_id)
+	) {$charset_collate};";
+	dbDelta( $sqlAnylnk );
+	dbDelta( $sqlIndex );
+	//add options
+	if( ! get_option( 'anylink_options' ) ) {
+		add_option( 'anylink_options', 
+			array( 
+					'version' => '0.12',
+					'redirectCat' => 'goto', 
+					'oldCat' => 'goto',
+					'redirectType' => '307',
+					'oldRedirectType' => '307',
+					'slugNum' => '4',
+					'oldSlugNum' => '4',
+					'slugChar' => '2',
+					'oldSlugChar' => '2' ),
+			'', 'no' );
+		//add and flush rewrite rule
+		add_rewrite_rule( "goto/([0-9a-z]{4,})", 'index.php?goto=$matches[1]', 'top' );
+		flush_rewrite_rules();
+	} else {
+		/* update option */
+		$al_option = get_option( 'anylink_options' );
+		if( ! isset( $al_option['version'] ) ) {
+			$al_option['version'] = '0.12';
+			$al_option['oldRedirectType'] = $al_option['redirectType'];
+			update_option( 'anylink_options', $al_option );
+		}
+	}		
+}
+function al_load_textdomain() {
+	$pluginDir = basename( dirname( __FILE__ ) );
+	load_plugin_textdomain( 'anylink', false, $pluginDir . '/i18n/' );
+}
+?>
