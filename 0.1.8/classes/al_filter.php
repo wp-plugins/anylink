7b16c451b2e4c46fa2aac6b45c14e4f6322e35c8
SivaDu <SivaDu@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-01-29 12:13:51 +0000

    updated version to 0.1.8
    git-svn-id: https://plugins.svn.wordpress.org/anylink/tags@847630 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ 0.1.8/classes/al_filter.php
@@ -0,0 +1,133 @@
+<?php
+class al_filter {
+	public $arrU2S = array();
+	public $redirectCat;
+	public $redirectType;
+    public $rel;
+	function __construct() {
+		$anylinkOptions = get_option( 'anylink_options' );
+		$this -> redirectCat  = $anylinkOptions['redirectCat'];
+		$this -> redirectType = $anylinkOptions['redirectType'];
+        $this -> rel = $anylinkOptions['rel'];
+	}
+	//@post_id: get all links and slugs of a specified post
+	public function getAllLnks( $post_id ) {
+		$arrURL = array();
+		global $wpdb;
+		$arrURL = $wpdb -> get_results( $wpdb -> prepare( 
+			"
+			SELECT U.al_id, U.al_slug,U.al_origURL 
+			FROM " . ANYLNK_DBINDEX . " I
+			LEFT JOIN " . ANYLNK_DBTB . " U
+			ON I.al_url_id = U.al_id
+			WHERE I.al_post_id = %d",
+			$post_id
+		), ARRAY_A );
+		return $arrURL;
+	}
+	//restore all URL
+	private function replaceURL( $matches ) {
+		$U2S = $this -> arrU2S;
+		$siteURL = home_url();
+        //$matches[2], $matches[5] already have key names: URL and rel
+        unset( $matches[2], $matches[5] );
+        //$mathes[0]  is the full matched string, delete it to use implode later
+        array_shift( $matches );
+		//only replace the links which have slugs, or return the original URL
+		if( array_key_exists( $matches['URL'], $U2S ) ) {
+            $matches['URL'] = $U2S[$matches['URL']];
+            //if need set rel manually
+            //get rel attributes which are already set in url and then merge them
+            if( ! empty( $this -> rel ) ) {
+                if( empty( $matches['rel'] ) )
+                    $rel = ' rel="' . $this -> rel . '"';
+                else {
+                    $_rel1 = explode( " ", $this -> rel );
+                    $_rel2 = explode( " ", $matches['rel'] );
+                    $_rel  = array_merge( $_rel1, $_rel2 );
+                    $_rel  = array_unique( $_rel );
+                    $rel   = implode( " ", $_rel );
+                }
+                $matches['rel'] = $rel;
+            }
+            return implode( '', $matches );
+        } else {
+            //if external link not indexed, or
+            //it is a interal link
+			return implode( '', $matches );
+        }
+	}
+	public function applyFilter( $content ) {
+		global $wp_query, $wp_rewrite;
+		$post_id = get_the_id();
+		$arrUrlSlug = $this -> getAllLnks( $post_id );
+		if( $arrUrlSlug ) {
+			foreach( $arrUrlSlug as $arrSlugs ) {
+				$this -> arrU2S[$arrSlugs['al_origURL']] = $this -> getInternalLinkBySlug( $arrSlugs['al_slug'] );
+			}
+		}
+		/*$pattern  = '#(<a\s*';
+		$pattern .= 'href=")(?P<URL>https?://[^"]+?)("';
+        $pattern .= '[^>]*?)(?<rel>rel="[^"]*")?';
+		$pattern .= '([^>]*?>)#i';*/
+        $pattern = '#(<a[^>]*?href=")(?<URL>https?://[^"]*)(")(?(?=[^>]*?rel=)([^>]*?rel=")(?<rel>[^"]*)([^>]*)|([^>]*))(>)#';
+		//covert ALL URLs, we can't just use str_replace, 
+		//coz a post may contain a plain URL, 
+		//even some text like this: href="http://dudo.org"
+		//Or, this plain text will be replaced as well
+		$content  = preg_replace_callback( $pattern, array( $this, 'replaceURL' ), $content );
+		return $content;
+	}
+	public function addQueryVars( $qvars ) {
+		array_push( $qvars, $this -> redirectCat );
+		return $qvars;
+	}
+	public function alter_the_query( $wp ) {
+		global $wp_query, $wp_rewrite;
+		$gotoURL = '';
+		if( $wp_rewrite -> using_permalinks() )
+			if ( array_key_exists( $this -> redirectCat, $wp -> query_vars ) && $wp -> query_vars[$this -> redirectCat] != '')  
+				$gotoURL = $wp -> query_vars[$this -> redirectCat];
+			else
+				return;
+		elseif( isset( $_GET[$this -> redirectCat] ))
+			$gotoURL = $_GET[$this -> redirectCat];
+		if( ! empty( $gotoURL ) ) {
+			wp_redirect( htmlspecialchars_decode( $this -> getUrlBySlug( $gotoURL ) ), ( int )$this -> redirectType );
+			exit;
+		}	
+	}
+	public function getUrlBySlug( $slug ) {
+		global $wpdb;
+		$URL = $wpdb -> get_var( $wpdb -> prepare( 
+			"
+			SELECT al_origURL
+			FROM " . ANYLNK_DBTB . " 
+			WHERE al_slug = %s",
+			$slug
+		) );
+		return $URL;
+	}
+	
+	public function getSlugById( $id ) {
+		global $wpdb;
+		$arrSlug = $wpdb -> get_row( $wpdb -> prepare( 
+			"SELECT * 
+			FROM " . ANYLNK_DBTB . "
+			WHERE al_id = %s", 
+			$id
+		), ARRAY_A );
+		return $arrSlug;
+	}
+	
+	public function getInternalLinkBySlug( $slug ) {
+		global $wp_rewrite;
+		$siteURL = home_url();
+		if( $wp_rewrite -> using_permalinks() )
+			$internalLink = $siteURL . '/'  . $this -> redirectCat . '/' . $slug;
+		else
+			$internalLink = $siteURL . '/?' . $this -> redirectCat . '=' . $slug;
+		return $internalLink;
+	}
+}
+?>
---
+++ 0.1.8/readme.txt
@@ -0,0 +1,106 @@
+﻿=== anyLink ===
+Contributors: SivaDu
+Donate link: http://dudo.org/
+Tags: seo, link sanitize, covert external links to internal links
+Requires at least: 3.4
+Tested up to: 3.8.1
+Stable tag: 0.1.8
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+AnyLink is a Wordpress plugin which allow you to customise you external link like an internal one.
+
+== Description ==
+
+Anylink allows you to covert the external links in your Wordpress to internal links. Of course, it's automatically. It's advantage
+is that Anylink Plugin doesn't destroy your data in Wordpress, which means once you removed it, you needn't do anything to your 
+posts.
+
+Also, you can customise the style of the link, such as its length, component, etc. You can customise the redirect type(http status) such as
+301, 307 as well.
+
+Mainly feature:
+
+*   covert external links to internal links, e.g. http://wordpress.org -> http://yourdomain/goto/a1b2
+*   customise the redirect category, e.g. you can change "goto" in the link above to any word you like
+*   allow you change the components of the slug, by default it's 4 letters and numbers. e.g. a1b2
+*   you can customise the redirect http status code, such as 301, 307
+
+== Frequently Asked Questions == 
+= What to do after installation? =
+Once Anylink is installed in your wordpress, you need running scan for the first time. Anylink will scan all your posts and grab all the
+external links.
+
+== Screenshots ==
+
+== Installation ==
+
+1. Upload `anylink.zip` to the `/wp-content/plugins/` directory
+2. Activate the plugin through the 'Plugins' menu in WordPress
+3. Scan all your post for the first time.
+
+== Changelog ==
+
+= 0.1.8 =
+*   Fixed a bug which cause fatal error
+*   修复了一个严重bug
+
+= 0.1.7 =
+*   Fixed some bugs may cause links missing
+*   optmized codes
+*   add the option which allows you set attribute REL of a link
+*   为链接增加了rel选项功能
+*   代码优化
+*   修复了一个bug，该bug可能引起anylink把内链误判为外链
+
+= 0.1.6 =
+*	Fixed some bugs cause 404 error
+*	Fixed a bug which may cause collison with other plugins
+*	修复了一些可能会引起插件造成404错误的bug
+*	修复了与其他插件存在潜在冲突的bug
+
+= 0.1.5 =
+*	Add a method anylink() you can call it anywhere. e.g. anylink( 'http://dudo.org', get_the_ID() ) you will get a coverted link
+*	another example: <? echo function_exists('anylink') ? anylink( $externalUrl, get_the_ID()) : $externalUrl; ?>
+*	增加了一个 anylink()方法，接受两个参数，第一个为需要转换的链接，第二个为文章ID，如果文章ID为空则默认为0
+*	使用方法如下 <? echo function_exists('anylink') ? anylink( $externalUrl, get_the_ID()) : $externalUrl; ?>
+
+= 0.1.4 =
+*	Fully support custom post types.
+*	You can custom which post types to be coverted which not.
+*	Most important, you needn't regenerate index once you change these settings
+*	Change log in Chinese below 以下为中文更新内容
+*	增加了对自定义类型文章的支持
+*	允许用户通过后台设置哪些类型的文章进行转换，哪些不需要进行转换
+*	最重要的是每次你更改这些设置时都不需要重新生成索引。
+
+= 0.1.3 =
+*	Fixed redirect problems, e.g. you can redirect url like http://dudo.org/.../url=http://...
+
+= 0.1.2 =
+*	Fixed some bugs in previous version
+*	Customed post type posts supported
+*	Javascript to redirect a page is available now
+
+= 0.1.1 =
+*	Fixed some bugs in v0.1
+*	Both English and Chinese languages are now supported
+*	POT file is supplied, so you can tranlate it into your own language as well
+
+= 0.1 =
+*	Covert all the external links to internal links by default
+*	Customise your link type
+*	Customise redirect http status code
+
+== Upgrade Notice ==
+
+=0.1.3=
+Fixed a bug which can cause url broken when redirecting
+
+=0.1.1=
+Fixed some bugs in v0.1
+Both English and Chinese languages are now supported
+POT file is supplied, so you can tranlate it into your own language as well
+
+=0.1=
+Main feature is developed.
